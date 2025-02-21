89e9db283addf4f8ac28dc165ff227259bb2aff6
Joaquin Franciscutti <joaco.1999.13@gmail.com>
2025-02-21 13:57:21 +0700

    fix env

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,12 +1,23 @@
-const dev = {
-  backEnd: "https://aed0-2001-fb1-d9-90f5-8d8a-5c46-9b4d-7206.ngrok-free.app/",
-  production: false,
+const environments = {
+  local: {
+    backEnd:
+      "https://6f08-2001-fb1-d9-90f5-c462-82a4-87b6-62ab.ngrok-free.app/",
+    production: false,
+  },
+  prod: {
+    backEnd: "https://league-cubb.fly.dev/",
+    production: true,
+  },
 };
 
-const _prod = {
-  backEnd: "https://league-cubb.fly.dev/",
-  production: true,
+const urlToEnvMap = {
+  localhost: environments.local,
+  "liga-cubb-home.vercel.app": environments.prod,
 };
 
-export const environment = dev;
-//export const environment = dev;
+const { hostname } = window.location;
+
+const defaultEnv = environments.local;
+
+export const environment =
+  urlToEnvMap[hostname as keyof typeof urlToEnvMap] || defaultEnv;
