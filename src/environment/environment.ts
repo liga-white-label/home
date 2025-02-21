82a0c75c4104450b1a787bb44f302191db5b6f09
Joaquin Franciscutti <joaco.1999.13@gmail.com>
2025-02-21 16:29:29 +0700

    fix env

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,7 +1,7 @@
 let baseUrl = "https://league-cubb.fly.dev/";
 
 if (process.env.NODE_ENV === "development") {
-  //baseUrl = "http://localhost:8080";
+  baseUrl = "http://localhost:8080";
 }
 
 export const environment = {
