af85112a73ceb38c8a8827a31a744f9f04fc7885
Joaquin Franciscutti <joaco.1999.13@gmail.com>
2025-04-08 21:45:29 -0300

    change gif por video

--- src/app/components/WelcomeGifContainer.tsx
+++ src/app/components/WelcomeGifContainer.tsx
@@ -3,21 +3,18 @@ import Image from "next/image";
 
 export const WelcomeGifContainer = () => {
   return (
-    <div className="flex flex-col items-center min-h-screen bg-[#220a0a] relative">
-      <Image
+    <div className="flex flex-col items-center h-[100svh] bg-[#220a0a] relative">
+      <video
         className="w-full h-screen object-cover"
-        draggable="false"
-        src="/assets/gif_welcome.gif"
-        alt=""
-        width={1920}
-        height={1080}
-        priority
-      />
-      {/* <div className="max-w-full h-[85svh] flex object-cover items-center justify-center text-center">
-        <p className="text-4xl md:text-8xl text-white">
-          Bienvenidos a la liga más linda del mundo
-        </p>
-      </div> */}
+        autoPlay
+        muted
+        loop
+        playsInline
+        preload="auto"
+      >
+        <source src="/assets/video-home.mp4" type="video/mp4" />
+        Tu navegador no soporta la etiqueta de video.
+      </video>
       <div className="absolute bottom-4 animate-bounce">
         <ArrowDropDown className="text-white h-10 w-10" />
       </div>
