2bdce0314566e184c02aa19d89fb263bbd286502
Marco Valla <marcovalla2014@gmail.com>
2025-08-22 23:51:14 -0300

    hotfix enviroment

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,30 +1,34 @@
 const environments = {
   local: {
-    backEnd: 'http://localhost:8081/',
+    baseUrl: "http://localhost:8081/",
     production: false,
   },
   ngrok: {
-    backEnd: 'https://de92-190-109-48-252.ngrok-free.app/',
+    baseUrl: "https://de92-190-109-48-252.ngrok-free.app/",
     production: false,
   },
   staging: {
-    backEnd: 'https://staging-api.ligacubb.com.ar/',
+    baseUrl: "https://staging-api.ligacubb.com.ar/",
     production: false,
   },
   prod: {
-    backEnd: 'https://api.ligacubb.com.ar/',
+    baseUrl: "https://api.ligacubb.com.ar/",
     production: true,
   },
 };
 
 const urlToEnvMap = {
   localhost: environments.local,
-  'ligacubb.com.ar': environments.prod,
   'staging-home.ligacubb.com.ar': environments.staging,
+  'ligacubb.com.ar': environments.prod,
 };
 
 const { hostname } = window.location;
 
 const defaultEnv = environments.local;
 
-export const environment = urlToEnvMap[hostname as keyof typeof urlToEnvMap] || defaultEnv;
+//export const environment = urlToEnvMap[hostname as keyof typeof urlToEnvMap] || defaultEnv;
+export const environment = {
+  production: false,
+  baseUrl: "https://staging-api.ligacubb.com.ar/",
+};
