0afbbfef52bbb38564ce7a9d8d50778d19aec3a6
Marco Valla <marcovalla2014@gmail.com>
2025-08-22 21:47:01 -0300

    Updating enviroment

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,10 +1,30 @@
-let baseUrl = "https://api.ligacubb.com.ar/";
-
-if (process.env.NODE_ENV === "development") {
-  baseUrl = "http://localhost:8081/";
-}
-
-export const environment = {
-  production: process.env.NODE_ENV === "production",
-  baseUrl,
+const environments = {
+  local: {
+    backEnd: 'http://localhost:8081/',
+    production: false,
+  },
+  ngrok: {
+    backEnd: 'https://de92-190-109-48-252.ngrok-free.app/',
+    production: false,
+  },
+  staging: {
+    backEnd: 'https://staging-api.ligacubb.com.ar/',
+    production: false,
+  },
+  prod: {
+    backEnd: 'https://api.ligacubb.com.ar/',
+    production: true,
+  },
 };
+
+const urlToEnvMap = {
+  localhost: environments.local,
+  'ligacubb.com.ar': environments.prod,
+  'staging-home.ligacubb.com.ar': environments.staging,
+};
+
+const { hostname } = window.location;
+
+const defaultEnv = environments.local;
+
+export const environment = urlToEnvMap[hostname as keyof typeof urlToEnvMap] || defaultEnv;
